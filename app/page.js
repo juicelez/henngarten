14c9ff869b312613dea489003466b4010d9c965e
juicelez <julius.henn@wattro.de>
2025-02-06 18:50:47 +0100

    add: bootstrap scss

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div className="container vh-100">
       hennfick
       <Header></Header>
+      <Button></Button>
     </div>
   );
 }
