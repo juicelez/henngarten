f9b5e3c5d94d43df99d771cb6aa962879cdb767d
juicelez <julius.henn@wattro.de>
2025-02-06 18:50:47 +0100

    add: bootstrap scss

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./global_styles.css";
-/* import 'bootstrap/dist/css/bootstrap.min.css'; */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/_custom.scss";
 
 export const metadata = {
   title: "Hennfick",
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div>
       hennfick
       <Header></Header>
+      <Button></Button>
     </div>
   );
 }
