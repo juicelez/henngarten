1fd6bb7fd6345871cb10e877b424bb624afdaae3
juicelez <julius.henn@wattro.de>
2025-02-07 13:17:46 +0100

    change: manipulate boostrap variables

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
     <div className="container vh-100">
       hennfick
       <Header></Header>
-      <button className="btn rounded btn-primary shadow">Click Me</button>
     </div>
   );
 }
