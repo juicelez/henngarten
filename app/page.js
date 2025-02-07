0c67c3ed065cef791ddc353ec1cff85315d315b4
juicelez <julius.henn@wattro.de>
2025-02-07 11:03:38 +0100

    wip: custom scss

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
-import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div>
       hennfick
       <Header></Header>
-      <Button></Button>
+      <button className="btn rounded btn-primary shadow">Click Me</button>
     </div>
   );
 }
