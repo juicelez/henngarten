7b1f99b0b1bbb8d32ace506fc429dcf92352df60
juicelez <111140175+juicelez@users.noreply.github.com>
2025-02-07 18:50:49 +0100

    Boostrap scss (#2)
    * add: bootstrap scss
    
    * wip: custom scss
    
    * change: manipulate boostrap variables
    
    * update: eslint-conifg-next

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Header from "./components/header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container vh-100">
       hennfick
       <Header></Header>
     </div>
