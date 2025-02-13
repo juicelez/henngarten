bb1baab626231b0b2d99f4ad50c5d76a5753affc
juicelez <julius.henn@wattro.de>
2025-02-13 08:12:14 +0100

    change: navbar for react-bootstrap

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ import styles from "./page.module.css";
 
 import { Container, Row, Col } from "react-bootstrap";
 // Components
-import Hero from "./components/hero"
-import Header from "./components/header"
-import Gartenbau from "./components/gartenbau"
-import Contactform from "./components/contactform"
-import Gartenpflege from "./components/gartenpflege"
-import Pflanzenverkauf from "./components/pflanzenverkauf"
+import Hero from "./components/hero";
+import Header from "./components/header";
+import Gartenbau from "./components/gartenbau";
+import Contactform from "./components/contactform";
+import Gartenpflege from "./components/gartenpflege";
+import Pflanzenverkauf from "./components/pflanzenverkauf";
 
 export default function Home() {
   useEffect(() => {
