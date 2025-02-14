5842d865f2f41528eaa63cf50b0868a99ef75434
Simo-Z <mail@simozilling.de>
2025-02-14 17:04:23 +0100

    feat: add contentSection component to simplyfy code (DRY)

--- app/page.js
+++ app/page.js
@@ -5,22 +5,19 @@ import { useEffect } from "react";
 // Components
 import Hero from "./components/hero";
 import Header from "./components/header";
-import Gartenbau from "./components/gartenbau";
 import Contactform from "./components/contactform";
-import Gartenpflege from "./components/gartenpflege";
+import ContentSection from "./components/contentSection";
 import Pflanzenverkauf from "./components/pflanzenverkauf";
 
+import contentblocks from "./data/content-blocks.json";
 export default function Home() {
-  useEffect(() => {
-    // Client-side only code here
-  }, []);
-
   return (
     <div className="vh-100">
       <Header />
       <Hero />
-      <Gartenbau />
-      <Gartenpflege />
+      {contentblocks.map((contentblock) => (
+        <ContentSection key={contentblock.id} {...contentblock} />
+      ))}
       <Pflanzenverkauf />
       <Contactform />
     </div>
