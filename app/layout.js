66f8688d94d7cb82c75116cacb3ac3e4b667ab1d
Simo-Z <mail@simozilling.de>
2025-02-15 10:59:45 +0100

    fix: adjust language

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="de">
       <body>{children}</body>
     </html>
   );
